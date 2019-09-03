46de819335d77f9490982f43350bea027ae62e01
Nand Kishore Sagi <NandKishore.Sagi@gmail.com>
2019-09-02 23:17:37 -0400

    Linked all data in data.js to index.html file.

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,13 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(record){
+    var row= tbody.append("tr");
+    Object.entries(record).forEach(function([key, value]){
+        //console.log(key, value)
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
